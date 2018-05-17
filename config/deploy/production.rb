a7f289840fa60acfc77862711eac1427c41d25b4
OkuyaKazuki <kazukiokuya@KazukiBook.local>
2018-05-17 15:53:19 +0900

    production.rbへの追記

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,4 @@
+server '54.248.56.22', user: 'ec2-user', roles: %w{app db web}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
