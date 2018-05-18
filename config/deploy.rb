ebbdae41b48eb7ccd1153fc84abb442e3f053a56
OkuyaKazuki <kazukiokuya@KazukiBook.local>
2018-05-18 14:10:38 +0900

    アプリケーション名の変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.10.2"
 
-set :application, 'hayabusa3703/story2'
+set :application, 'story2'
 set :repo_url,  'git@github.com:hayabusa3703/story2.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
