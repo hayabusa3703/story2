01eae3d08790aa7f22d8321e526a7066b0ca0c36
kamikura <yusuke@yusuke-no-MacBook-Air.local>
2018-05-19 11:34:39 +0900

    Add routes
    Story2に移行したため、再度追加。

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,35 @@ Rails.application.routes.draw do
   get  'storys/:id/edit' => 'storys#edit'
   get  'newest' => 'newest#index'
 
+  post  'category/love'      =>  'storys#create'
+  post  'category/marry'     =>  'storys#create'
+  post  'category/study'     =>  'storys#create'
+  post  'category/work'      =>  'storys#create'
+  post  'category/newlife'   =>  'storys#create'
+  post  'category/trip'      =>  'storys#create'
+  post  'category/sick'      =>  'storys#create'
+  post  'category/daily'     =>  'storys#create'
+
+  get  'category/love/newest' => 'love#index'
+    # loveカテゴリで新着順にする。love indexに記述？　newestに記述？
+  get  'newest' => 'newest#index'
+  get  'category/love' => 'love#index'
+  get  'category/marry' => 'marry#index'
+  get  'category/study' => 'study#index'
+  get  'category/work' => 'work#index'
+  get  'category/newlife' => 'newlife#index'
+  get  'category/trip' => 'trip#index'
+  get  'category/sick' => 'sick#index'
+  get  'category/daily' => 'daily#index'
+
+
+
+
+
+
+
+
+
   get 'latest' => 'latest#index'
   get 'legend' => 'legend#index'
 end
