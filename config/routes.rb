73f9b454cf0f6a702763ef093bdd7701fcb2d5a3
kamikura <yusuke@yusuke-no-MacBook-Air.local>
2018-05-21 18:03:57 +0900

    Delete unrelated files

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
-  # get 'categories/index'
-
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#index'
     resources :storys do
       resources :likes, only: [:create, :destroy]
     end
-
   get   'storys'  =>  'storys#index'
   get   'storys/new'  =>  'storys#new'
   post  'storys'      =>  'storys#create'
@@ -15,8 +12,6 @@ Rails.application.routes.draw do
   patch  'storys/:id' => 'storys#update'
   get  'storys/:id/edit' => 'storys#edit'
   get  'newest' => 'newest#index'
-
-  # post  'category/love'      =>  'storys#create'
   get  'category' => 'categorys#index'
 
   get 'latest' => 'latest#index'
