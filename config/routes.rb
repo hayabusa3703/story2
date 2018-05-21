c2ef1022ae6ce85c052c3aa141c782828e41902a
kamikura <yusuke@yusuke-no-MacBook-Air.local>
2018-05-21 17:48:43 +0900

    Correction routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'categories/index'
-
-  get 'battle/test'
+  # get 'categories/index'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -18,22 +16,8 @@ Rails.application.routes.draw do
   get  'storys/:id/edit' => 'storys#edit'
   get  'newest' => 'newest#index'
 
-  post  'category/love'      =>  'storys#create'
-  post  'category/marry'     =>  'storys#create'
-  post  'category/study'     =>  'storys#create'
-  post  'category/work'      =>  'storys#create'
-  post  'category/newlife'   =>  'storys#create'
-  post  'category/trip'      =>  'storys#create'
-  post  'category/sick'      =>  'storys#create'
-  post  'category/daily'     =>  'storys#create'
-
-  get  'category' => 'categories#index'
-    # loveカテゴリで新着順にする。love indexに記述？　newestに記述？
-
-
-
-
-
+  # post  'category/love'      =>  'storys#create'
+  get  'category' => 'categorys#index'
 
   get 'latest' => 'latest#index'
   get 'legend' => 'legend#index'
