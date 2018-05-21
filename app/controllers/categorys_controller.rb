e74dacd55d8cdae5be05c0bf5db61d5275786d0d
kamikura <yusuke@yusuke-no-MacBook-Air.local>
2018-05-21 17:52:59 +0900

    Change category name

---
+++ app/controllers/categorys_controller.rb
@@ -0,0 +1,18 @@
+class CategorysController < ApplicationController
+
+  def index
+    @categorys = Category.all
+    @category = Category.find(params[:format])
+    @storys = Story.where(category_id: params[:format])
+  end
+
+ private
+  def story_params
+    params.permit(:title, :category_id, :text).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
+
+end
