78863d013eba3c9395caeda6d521fc4b9ed29a68
kamikura <yusuke@yusuke-no-MacBook-Air.local>
2018-05-22 19:33:30 +0900

    Fixed controller

--- app/controllers/categorys_controller.rb
+++ app/controllers/categorys_controller.rb
@@ -3,23 +3,21 @@ class CategorysController < ApplicationController
   before_action :set_category, only: [:index, :show, :newest]
 
   def index
-    @categorys = Category.all
-    @storys = Story.where(category_id: params[:format])
+    @storys = Story.where(category_id: params[:id])
   end
 
   def show
-    @categorys = Category.all
     @storys = Story.where(category_id: params[:id])
   end
 
   def newest
-    @categorys = Category.all
     @storys = Story.where(category_id: params[:id]).order("created_at DESC")
   end
 
   private
 
     def set_category
+      @categorys = Category.all
       @category = Category.find(params[:id])
     end
 
