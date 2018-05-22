5bc82ead934848839fe483fdbd409f7b3c9f9e4b
kamikura <yusuke@yusuke-no-MacBook-Air.local>
2018-05-22 16:36:31 +0900

    Edit routes
    resourcesを使用して編集。
    categorysにshowコントローラーを追加。

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,20 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#index'
-    resources :storys do
-      resources :likes, only: [:create, :destroy]
+  resources :storys do
+    resources :likes, only: [:create, :destroy]
+    resources :new, only: [:index, :new]
+  end
+
+  resources :categorys , only: [:index, :show] do
+    member do
+      get :newest
     end
-  get   'storys'  =>  'storys#index'
-  get   'storys/new'  =>  'storys#new'
-  post  'storys'      =>  'storys#create'
+  end
+
   delete 'storys/:id' =>  'storys#destroy'
   patch  'storys/:id' => 'storys#update'
   get  'storys/:id/edit' => 'storys#edit'
-  get  'newest' => 'newest#index'
-  get  'category' => 'categorys#index'
-
   get 'latest' => 'latest#index'
   get 'legend' => 'legend#index'
 end
