108ef8c0320ce68e678c2cf4c9c37d457bf27a03
kamikura <yusuke@yusuke-no-MacBook-Air.local>
2018-05-22 16:55:43 +0900

    Delete test files

--- test/controllers/battle_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class BattleControllerTest < ActionDispatch::IntegrationTest
-  test "should get test" do
-    get battle_test_url
-    assert_response :success
-  end
-
-end
