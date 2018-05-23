d0ec308b9a08d11f5714193fa98e968c0fc062d5
kamikura <yusuke@yusuke-no-MacBook-Air.local>
2018-05-23 19:13:13 +0900

    Delete files
    不要なコントローラーのため削除。

--- app/controllers/newest_controller.rb
+++
@@ -1,8 +0,0 @@
-class NewestController < ApplicationController
-
-  def index
-    @categorys = Category.all
-    @category = Category.find(params[:category_id])
-    @storys = Story.all.order("id DESC").page(params[:page]).per(4)
-  end
-end
