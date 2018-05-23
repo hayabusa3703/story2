3fd379c8e8276b8c2836e5bde7c8032cc67199a5
kamikura <yusuke@yusuke-no-MacBook-Air.local>
2018-05-23 19:14:06 +0900

    ページ上の表示ストーリー数を定数として定義。

--- app/controllers/latest_controller.rb
+++ app/controllers/latest_controller.rb
@@ -1,7 +1,9 @@
 class LatestController < ApplicationController
+  LATESTSTORYS = 12
+
   def index
     @story = Story.new
-    @storys = Story.all.page(params[:page]).per(12).reverse
+    @storys = Story.all.page(params[:page]).per(LATESTSTORYS).order('created_at DESC')
     @categorys = Category.all
   end
 end
