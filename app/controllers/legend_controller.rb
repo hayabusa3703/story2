812d8ad273cb25ca318001779f6e3c7074d4f45f
kamikura <yusuke@yusuke-no-MacBook-Air.local>
2018-05-24 13:15:55 +0900

    Change legend category
    表示されるストーリー数を変更。

--- app/controllers/legend_controller.rb
+++ app/controllers/legend_controller.rb
@@ -1,5 +1,9 @@
 class LegendController < ApplicationController
+  LEGENDSTORYS = 12
+
   def index
+    @story = Story.new
+    @storys = Story.all.page(params[:page]).per(LEGENDSTORYS).order('created_at DESC')
     @categorys = Category.all
   end
 end
