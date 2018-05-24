47e9af56118c6aedad38f199440981099c935239
kamikura <yusuke@yusuke-no-MacBook-Air.local>
2018-05-24 13:12:25 +0900

    Define number
    トップページ上部に表示されるストーリー数を定義した。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
+  TOPSTORYS = 4
   def index
+    @story = Story.new
+    @storys = Story.all.page(params[:page]).per(TOPSTORYS).order('created_at DESC')
     @categorys = Category.all
   end
 end
