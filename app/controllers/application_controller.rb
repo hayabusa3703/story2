121dda490a41c753de5c2464aaf6a42f4a3d3d4f
kamikura <yusuke@yusuke-no-MacBook-Air.local>
2018-05-25 15:40:31 +0900

    Delete description
    不要な記述のため削除した。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   TOPSTORYS = 4
   def index
-    @story = Story.new
     @storys = Story.page(params[:page]).per(TOPSTORYS).by_created_at
     @categorys = Category.all
   end
