8cc993384cf05618cca613a36cd198c1ef853c76
kamikura <yusuke@yusuke-no-MacBook-Air.local>
2018-05-25 15:38:57 +0900

    Add comment out

--- app/controllers/legend_controller.rb
+++ app/controllers/legend_controller.rb
@@ -1,6 +1,8 @@
 class LegendController < ApplicationController
   LEGENDSTORYS = 12
 
+# まだ未実装の機能のため、この形で一時保存しています。
+
   def index
     @story = Story.new
     @storys = Story.all.page(params[:page]).per(LEGENDSTORYS).order('created_at DESC')
