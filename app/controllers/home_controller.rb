ea62d7ca4fa645df7f00f100f6dfe45221a863ae
hayabusa3703 <umibudou3703@gmail.com>
2018-05-28 14:31:28 +0900

    ページネーションのビューの編集

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 
 PROFILESTORYS = 6
 
-    def index
+  def index
     @categorys = Category.all
     @story = Story.new
     @storys = Story.page(params[:page]).per(PROFILESTORYS).by_created_at
