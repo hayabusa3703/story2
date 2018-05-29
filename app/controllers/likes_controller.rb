ad9dfac5997181cf3122fcbde3847cd71ec68eee
hayabusa3703 <umibudou3703@gmail.com>
2018-05-29 14:03:48 +0900

    コントローラの作成

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,19 +1,65 @@
 class LikesController < ApplicationController
-   before_action :like_where, only: [:create, :destroy]
+  before_action :set_variables
 
-  def create
-    @like = Like.create(user_id: current_user.id, story_id: params[:story_id])
-    @storys = Story.all
+  def like
+    like = current_user.likes.new(story_id: @story.id)
+    like.save
   end
 
-  def destroy
-    @like = Like.find_by(user_id: current_user.id, story_id: params[:story_id])
-    @like.destroy
-    @storys = Story.all
+  def unlike
+    like = current_user.likes.find_by(story_id: @story.id)
+    like.destroy
   end
 
   private
-  def like_where
-    @likes = Like.where(story_id: params[:story_id])
+
+  def set_variables
+    @story = Story.find(params[:story_id])
+    @id_name = "#like-link-#{@story.id}"
+    @id_heart = "#heart-#{@story.id}"
   end
+
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class LikesController < ApplicationController
+#    before_action :like_where, only: [:create, :destroy]
+
+#   def create
+#     @like = Like.create(user_id: current_user.id, story_id: params[:story_id])
+#     @storys = Story.all
+#   end
+
+#   def destroy
+#     @like = Like.find_by(user_id: current_user.id, story_id: params[:story_id])
+#     @like.destroy
+#     @storys = Story.all
+#   end
+
+#   private
+#   def like_where
+#     @likes = Like.where(story_id: params[:story_id])
+#   end
+# end
