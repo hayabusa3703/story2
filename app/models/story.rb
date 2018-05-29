db486197177930bd6dfa88e1dec3ab57aab87437
hayabusa3703 <umibudou3703@gmail.com>
2018-05-29 14:02:21 +0900

    アソシエーションの定義

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,7 +1,8 @@
 class Story < ApplicationRecord
   belongs_to :user
   belongs_to :category
-  has_many :likes
+  has_many :likes, dependent: :destroy
+  has_many :liking_users, through: :likes, source: :user
   has_many :comments
   scope :by_created_at, -> { order('created_at DESC') }
 end
