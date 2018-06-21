4a576015fba5cedcbf051cb248f575b9d14f77d5
hayabusa3703 <umibudou3703@gmail.com>
2018-06-21 15:33:47 +0900

    不要な記述の削除

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -16,50 +16,6 @@ class LikesController < ApplicationController
   def set_variables
     @story = Story.find(params[:story_id])
     @id_name = "#like-link-#{@story.id}"
-    @id_heart = "#heart-#{@story.id}"
   end
 
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# class LikesController < ApplicationController
-#    before_action :like_where, only: [:create, :destroy]
-
-#   def create
-#     @like = Like.create(user_id: current_user.id, story_id: params[:story_id])
-#     @storys = Story.all
-#   end
-
-#   def destroy
-#     @like = Like.find_by(user_id: current_user.id, story_id: params[:story_id])
-#     @like.destroy
-#     @storys = Story.all
-#   end
-
-#   private
-#   def like_where
-#     @likes = Like.where(story_id: params[:story_id])
-#   end
-# end
