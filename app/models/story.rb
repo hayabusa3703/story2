5425544437eb33f6cdbc076df9a4f8ad6bce9866
hayabusa3703 <umibudou3703@gmail.com>
2018-06-26 11:23:21 +0900

    不要な空行の削除など

--- app/models/story.rb
+++ app/models/story.rb
@@ -2,7 +2,6 @@ class Story < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_many :likes, dependent: :destroy
-  has_many :liking_users, through: :likes, source: :user
   has_many :comments
   scope :by_created_at, -> { order('created_at DESC') }
 end
