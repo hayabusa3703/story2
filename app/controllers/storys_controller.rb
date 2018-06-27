d9018ef72bddd24d8631fa3bce87148a8ba4b728
hayabusa3703 <umibudou3703@gmail.com>
2018-06-27 16:15:29 +0900

    リダイレクト先の変更

--- app/controllers/storys_controller.rb
+++ app/controllers/storys_controller.rb
@@ -50,7 +50,7 @@ class StorysController < ApplicationController
   end
 
   def move_to_index
-    redirect_to action: :index unless user_signed_in?
+    redirect_to root_url unless user_signed_in?
   end
 
   def set_story
