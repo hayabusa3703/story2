8a9ae373e569a05e41dc1fce6de79682d948c351
hayabusa3703 <umibudou3703@gmail.com>
2018-07-03 17:38:32 +0900

    勉強、仕事のストーリーをカテゴリ別にトップページに出せるように設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,10 @@ class ApplicationController < ActionController::Base
     @love = Story.where(Category_id: 1).limit(1)
     @marriages = Story.where(Category_id: 2).limit(CATEGORYSTORYS)
     @marriage = Story.where(Category_id: 2).limit(1)
+    @studies = Story.where(Category_id: 3).limit(CATEGORYSTORYS)
+    @study = Story.where(Category_id: 3).limit(1)
+    @jobs = Story.where(Category_id: 4).limit(CATEGORYSTORYS)
+    @job = Story.where(Category_id: 4).limit(1)
   end
 
 	before_action :configure_permitted_parameters, if: :devise_controller?
