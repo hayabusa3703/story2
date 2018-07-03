0e1d59a1289e66c1a3b93e7b409b794d792742ba
hayabusa3703 <umibudou3703@gmail.com>
2018-07-03 17:21:26 +0900

    恋愛、結婚のストーリーをカテゴリ別にトップページに出せるように設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   TOPSTORYS = 4
+  CATEGORYSTORYS = 3
+
   def index
     @storys = Story.page(params[:page]).per(TOPSTORYS).by_created_at
     @categorys = Category.all
+    @loves = Story.where(Category_id: 1).limit(CATEGORYSTORYS)
+    @love = Story.where(Category_id: 1).limit(1)
+    @marriages = Story.where(Category_id: 2).limit(CATEGORYSTORYS)
+    @marriage = Story.where(Category_id: 2).limit(1)
   end
 
 	before_action :configure_permitted_parameters, if: :devise_controller?
