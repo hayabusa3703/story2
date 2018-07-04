49bce6a3bd8f3ab46c6a6d03ec753022c4e06c3d
kamikura <yusuke@yusuke-no-MacBook-Air.local>
2018-07-04 12:11:30 +0900

    並び替え機能にページネーションを追加

--- app/controllers/categorys_controller.rb
+++ app/controllers/categorys_controller.rb
@@ -1,18 +1,14 @@
 class CategorysController < ApplicationController
 
   before_action :set_category, only: [:index, :show, :newest, :oldest, :popular]
-  before_action :set_sort, only: [:index, :show]
-
-  def newest
-    @storys = Story.where(category_id: params[:id]).order("created_at DESC")
-  end
+  before_action :set_sort, only: [:index, :show, :newest]
 
   def oldest
-    @storys = Story.where(category_id: params[:id]).order("created_at ASC")
+    @storys = Story.where(category_id: params[:id]).page(params[:page]).per(12).order('created_at ASC')
   end
 
   def popular
-    @storys = Story.where(category_id: params[:id]).order("likes_count DESC")
+    @storys = Story.where(category_id: params[:id]).page(params[:page]).per(12).order('likes_count DESC')
   end
 
   private
@@ -23,7 +19,6 @@ class CategorysController < ApplicationController
     end
 
     def set_sort
-      @storys = Story.where(category_id: params[:id])
-      @story = Story.all.page(params[:page]).per(2).order('created_at DESC')
+      @storys = Story.where(category_id: params[:id]).page(params[:page]).per(12).order('created_at DESC')
     end
 end
