6588d2ba14da916235cf618b3f20c8c4f6fc21d6
hayabusa3703 <umibudou3703@gmail.com>
2018-07-04 18:51:23 +0900

    不要なコードの削除と、簡潔な書き方への変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,22 +6,14 @@ class ApplicationController < ActionController::Base
   def index
     @storys = Story.page(params[:page]).per(TOP_STORIES_COUNT).by_created_at
     @categorys = Category.all
-    @loves = Story.where(Category_id: 1).limit(TOP_CATEGORYSTORYS_COUNT)
-    @love = Story.where(Category_id: 1).limit(1)
-    @marriages = Story.where(Category_id: 2).limit(TOP_CATEGORYSTORYS_COUNT)
-    @marriage = Story.where(Category_id: 2).limit(1)
-    @studies = Story.where(Category_id: 3).limit(TOP_CATEGORYSTORYS_COUNT)
-    @study = Story.where(Category_id: 3).limit(1)
-    @jobs = Story.where(Category_id: 4).limit(TOP_CATEGORYSTORYS_COUNT)
-    @job = Story.where(Category_id: 4).limit(1)
-    @newlifes = Story.where(Category_id: 5).limit(TOP_CATEGORYSTORYS_COUNT)
-    @newlife = Story.where(Category_id: 5).limit(1)
-    @travels = Story.where(Category_id: 6).limit(TOP_CATEGORYSTORYS_COUNT)
-    @travel = Story.where(Category_id: 6).limit(1)
-    @sicks = Story.where(Category_id: 7).limit(TOP_CATEGORYSTORYS_COUNT)
-    @sick = Story.where(Category_id: 7).limit(1)
-    @everydaylifes = Story.where(Category_id: 8).limit(TOP_CATEGORYSTORYS_COUNT)
-    @everydaylife = Story.where(Category_id: 8).limit(1)
+    @loves = Story.love.limit(TOP_CATEGORYSTORYS_COUNT)
+    @marriages = Story.marriage.limit(TOP_CATEGORYSTORYS_COUNT)
+    @studies = Story.study.limit(TOP_CATEGORYSTORYS_COUNT)
+    @jobs = Story.job.limit(TOP_CATEGORYSTORYS_COUNT)
+    @newlifes = Story.newlife.limit(TOP_CATEGORYSTORYS_COUNT)
+    @travels = Story.travel.limit(TOP_CATEGORYSTORYS_COUNT)
+    @sicks = Story.sick.limit(TOP_CATEGORYSTORYS_COUNT)
+    @everydaylifes = Story.everydaylife.limit(TOP_CATEGORYSTORYS_COUNT)
   end
 
 	before_action :configure_permitted_parameters, if: :devise_controller?
